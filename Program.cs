9e81757b8f8697a3054abf18af3bfb207cb00238
Al Khwarizmi <nfg214@icloud.com>
2018-08-18 21:00:26 -0500

    Add Class6_UpperCaseAssignment.csproj and Program.cs

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Class6_UpperCaseAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello, type a sentence: ");
+            //The user's entry is assigned to string 'userInput'
+            string userInput = Console.ReadLine();
+
+            //'TurnToUpper' is invoked with 'userInput' string. 
+            //The method and its resulting value is interpolated in the WriteLine. 
+            Console.WriteLine($"Your sentence in upper case: {TurnToUpper(userInput)}");
+          
+            Console.ReadLine();
+        }
+
+        //Created local method 'TurnToUpper' to pass user's entry through method code
+        private static string TurnToUpper(string message)
+        {
+            //method code converts the user's string to all upper case
+            return (message.ToUpper());
+        }
+    }
+}
